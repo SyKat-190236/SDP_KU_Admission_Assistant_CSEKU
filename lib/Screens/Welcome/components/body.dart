3f3133df40ecd157f19f2d344ee1e919dc56f50d
Sykat Biswas <53664820+SyKat-190236@users.noreply.github.com>
2022-02-14 23:42:13 +0600

    login_signup_completed

---
+++ lib/Screens/Welcome/components/body.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/Screens/Login/login_screen.dart';
+import 'package:flutter_auth/Screens/Signup/signup_screen.dart';
+import 'package:flutter_auth/Screens/Welcome/components/background.dart';
+import 'package:flutter_auth/components/rounded_button.dart';
+import 'package:flutter_auth/constants.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "KU Admission Assistant",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+            ),
+            SizedBox(height: size.height * 0.05),
+            Image.asset(
+              "assets/icons/ku.jpg",
+            ),
+            SizedBox(height: size.height * 0.05),
+            RoundedButton(
+              text: "LOGIN",
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return LoginScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+            RoundedButton(
+              text: "SIGN UP",
+              color: kPrimaryLightColor,
+              textColor: Colors.black,
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return SignUpScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
